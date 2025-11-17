ab12c33869d8617d50b5b1dc263a111107a16806
mark-summerfield <mark@qtrac.eu>
2025-11-17 11:30:59 +0000

    m

--- st.sh
+++ st.sh
@@ -6,6 +6,8 @@ nagelfar.sh \
     | grep -v Variable.*is.never.read \
     | grep -v Unknown.subcommand..home..to..file \
     | grep -v Found.constant.*which.is.also.a.variable
+du -sh .git
+ls -sh .*.str
 clc -s -l tcl
 str s
 git st
